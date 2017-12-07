6a6ed6db36959751c38c8480a7964bd7ef7cc9a2
souravsharmaaa <34339688+souravsharmaaa@users.noreply.github.com>
2017-12-07 17:06:26 +0530

    Update example.py

--- example.py
+++ example.py
@@ -13,17 +13,17 @@ from instabot import InstaBot
 from unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
-    login="username",
-    password="password",
+    login="s.o.u.r.a.v.s",
+    password="astro@123",
     like_per_day=1000,
-    comments_per_day=0,
-    tag_list=['follow4follow', 'f4f', 'cute'],
+    comments_per_day=200,
+    tag_list=['follow4follow', 'shoot', 'cute'],
     tag_blacklist=['rain', 'thunderstorm'],
     user_blacklist={},
-    max_like_for_one_tag=50,
+    max_like_for_one_tag=200,
     follow_per_day=300,
     follow_time=1 * 60,
-    unfollow_per_day=300,
+    unfollow_per_day=3,
     unfollow_break_min=15,
     unfollow_break_max=30,
     log_mod=0,
